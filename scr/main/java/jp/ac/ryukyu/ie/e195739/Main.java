837fd6e7b8d7b86ac58be737140b573c5c27c457
zhangjiaxin0913 <e195739@ie.u-ryukyu.ac.jp>
2021-02-07 20:00:39 +0900

    player側の値の入力

--- scr/main/java/jp/ac/ryukyu/ie/e195739/Main.java
+++ scr/main/java/jp/ac/ryukyu/ie/e195739/Main.java
@@ -1,6 +1,7 @@
 package jp.ac.ryukyu.ie.e195739;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
@@ -8,5 +9,11 @@ public class Main {
         Random r = new Random();
         int i1 = r.nextInt(100); //1~100のかなの一個任意な値を生成する
         System.out.println("ランダムの値は" + i1);
+        System.out.println("1-100の間、整数を入力してください。");
+        Scanner sc=new Scanner(System.in);//playerが数値を入力する
+        while(true) {
+            int b = sc.nextInt();
+        }
+
     }
 }
