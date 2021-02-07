35920325ddcec11955daf800bac43da4c560741c
zhangjiaxin0913 <e195739@ie.u-ryukyu.ac.jp>
2021-02-07 20:11:17 +0900

    当てた数と乱数を比較する

--- scr/main/java/jp/ac/ryukyu/ie/e195739/Main.java
+++ scr/main/java/jp/ac/ryukyu/ie/e195739/Main.java
@@ -8,12 +8,22 @@ public class Main {
     public static void main(String[] args){
         Random r = new Random();
         int i1 = r.nextInt(100); //1~100のかなの一個任意な値を生成する
-        System.out.println("ランダムの値は" + i1);
+
+        //System.out.println("ランダムの値は" + i1);
+
         System.out.println("1-100の間、整数を入力してください。");
         Scanner sc=new Scanner(System.in);//playerが数値を入力する
-        while(true) {
-            int b = sc.nextInt();
+        Loop:
+        while (true) {                   // 数回繰り返す
+            int result = sc.nextInt();
+            if (result < i1) {
+                System.out.println("もう少し大きい");
+            } else if (result > i1) {
+                System.out.println("もう少し小さい");
+            } else {
+                System.out.println("bingo");
+                break;                    //当てた数と乱数が同じので、ループを終了します。
+            }
         }
-
     }
 }
